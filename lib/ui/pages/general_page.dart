38ea189622edd74d2e7a291a495acd4a63ba2ef6
khairul anas <mafiakanan@gmail.com>
2020-12-06 15:31:24 +0700

    general page

---
+++ lib/ui/pages/general_page.dart
@@ -0,0 +1,85 @@
+part of 'pages.dart';
+
+class GeneralPage extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final Function onBackButtonPressed;
+  final Widget child;
+  final Color backColor;
+
+  const GeneralPage({
+    Key key,
+    this.title,
+    this.subtitle,
+    this.onBackButtonPressed,
+    this.backColor,
+    this.child,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Container(
+          color: Colors.white,
+        ),
+        SafeArea(
+          child: Container(
+            color: backColor ?? 'FAFAFC'.toColor(),
+          ),
+        ),
+        SafeArea(
+          child: ListView(
+            children: [
+              Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: defaultMargin),
+                    padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+                    width: double.infinity,
+                    height: 100,
+                    color: Colors.white,
+                    child: Row(
+                      children: [
+                        onBackButtonPressed != null
+                            ? Container(
+                                width: 24,
+                                height: 24,
+                                margin: EdgeInsets.only(right: 26),
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(
+                                            'assets/back_arrow.png'))),
+                              )
+                            : SizedBox(),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              title,
+                              style: GoogleFonts.poppins(
+                                  fontSize: 22, fontWeight: FontWeight.w500),
+                            ),
+                            Text(
+                              subtitle,
+                              style: GoogleFonts.poppins(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w300,
+                                  color: greyColor),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  child ?? SizedBox()
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    ));
+  }
+}
